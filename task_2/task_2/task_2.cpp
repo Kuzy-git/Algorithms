ad39a8c23bcbe3fbe0f0828641ed6367b20b6a63
Татьяна <90142707+Kuzy-git@users.noreply.github.com>
2022-01-24 00:07:52 +0500

    поменяла 2.2 и 2.4

--- task_2/task_2/task_2.cpp
+++ task_2/task_2/task_2.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <map>
 using namespace std;
 
 ///////// Задание 1
@@ -36,49 +37,29 @@ void task_1()
 
 //////// Задание 2
 
-void input(vector<int> &arr, int &n) /// Функция заполнения массива числами 
-{      
-    cout << "Заполните массив числами от 1 до 20:" << endl;
+
+void task_2()
+{
+    int n;
+    
+    cout << "\nВведите натуральное число - количество чисел на ввод:" << endl;
+    cin >> n;
+    cout << endl;
+
+    map<int, int> dict;
+    vector<int> v(n);    
+    
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
-        while ((arr[i] < 1) || (arr[i] > 20))
-        {
-            cout << "Вы ввели число вне диапазона [1; 20]. Попробуйте снова:" << endl;
-            cin >> arr[i];
-        }
+        cin >> v[i];
+        dict[v[i]] += 1;
     }
+   
+    for (auto digit : dict)
+        cout << digit.first << ":" << digit.second << endl;
 }
 
-void sort(vector<int>& arr, int &n) /// Сортировка массива по возрастанию
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        int min = i;
-        
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[min])
-                min = j;
-        }
-        swap(arr[i], arr[min]);
-    }
-}
 
-void print(vector<int>& arr, int& n) /// Вывод чисел
-{
-    int k = 1;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == arr[i + 1])
-            k += 1;
-        else
-        {
-            cout << arr[i] << ":" << k << endl;
-            k = 1;
-        }
-    }
-}
 
 /// /// Задание 3
 
@@ -118,12 +99,8 @@ int main()
      task_1();
        
     /// Задание 2
-    cout << "\nВведите натуральное число - размер массива:" << endl;
-    cin >> n;
-    vector<int> a(n+1);
-    input(a, n); 
-    sort(a, n);  
-    print(a, n);
+    
+     task_2();
    
     /// Задание 3
     ofstream fout("output.txt");
@@ -138,15 +115,14 @@ int main()
     //// Задание 4
     vector<int> mass;
     int f;
-    while (true)
-    {
-        cout << "\n" << "Введите число: ";
-        cin >> f;
-        mass.push_back(f);
-        cout << "Capacity:  " << mass.capacity() << endl;
-        cout << "Size: " << mass.size() << endl;
-      
-    }
+    cout << "\n" << "Введите число: ";
+    cin >> f;
+    for (int i=0; i<f; i++)   
+    {              
+        cout << "Size: " << mass.size() << " \tCapacity:  " << mass.capacity() << endl;  
+        mass.push_back(1);
+    }  
+
     return 0;
 }
 
